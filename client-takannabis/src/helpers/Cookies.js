db01ecc00baf80d8d34ce606e5ecbcbbc7ea36d3
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2018-03-17 12:37:35 -0400

    added JWT token helpers for components

---
+++ client-takannabis/src/helpers/Cookies.js
@@ -0,0 +1,42 @@
+// in react world, using ES6 exports
+export default {
+
+  // Methods to get and set cookies from W3 Schools
+  // https://www.w3schools.com/js/js_cookies.asp
+
+  // get cookie given the cookie's name
+  get(cname) {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    // eslint says foreach is better approach
+    ca.forEach((el, i) => {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+      return ''; // should this return be outside???????????? VV one line down?
+    });
+    // for (let i = 0; i < ca.length; i++) {
+    //   let c = ca[i];
+    //   while (c.charAt(0) === ' ') {
+    //     c = c.substring(1);
+    //   }
+    //   if (c.indexOf(name) === 0) {
+    //     return c.substring(name.length, c.length);
+    //   }
+    // }
+    // return '';
+  },
+
+  // set cookie given the cookie's name, value and time*optional
+  set(cname, cvalue, exdays = 30) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = `expires=${d.toUTCString()}`;
+    document.cookie = `${cname}=${cvalue};${expires};path=/`;
+  },
+};
